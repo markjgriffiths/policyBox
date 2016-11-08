2bd1bf5e6157c513cce8357d4a1a93b6c7a68ccc
markjgriffiths <mark.j.griffiths@live.co.uk>
2016-11-08 22:56:29 +0000

    New Project
    All as new

---
+++ policyBox/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  policyBox
+//
+//  Created by Mark Griffiths on 08/11/2016.
+//  Copyright © 2016 MarkGriffiths. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
